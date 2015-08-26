e8781f3dc228833241c073cf211d4ff225814176
Josh Zacharias <josh.owie@gmail.com>
2015-08-26 16:50:34 -0700

    Add some assets for displaying an icon in the process descriptions.

---
+++ src/main/java/org/terasology/eventualSkills/ui/EventualSkillsUIUtil.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2015 MovingBlocks
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.terasology.eventualSkills.ui;
+
+import org.terasology.asset.Assets;
+import org.terasology.assets.ResourceUrn;
+import org.terasology.eventualSkills.components.EventualSkillDescriptionComponent;
+import org.terasology.eventualSkills.systems.EventualSkillsManager;
+import org.terasology.registry.CoreRegistry;
+import org.terasology.rendering.nui.UIWidget;
+import org.terasology.rendering.nui.widgets.UIImage;
+import org.terasology.rendering.nui.widgets.UILabel;
+
+public final class EventualSkillsUIUtil {
+    public static UIWidget createEventualSkillsIcon(ResourceUrn skillUrn) {
+        EventualSkillsManager eventualSkillsManager = CoreRegistry.get(EventualSkillsManager.class);
+        EventualSkillDescriptionComponent skillDescriptionComponent = eventualSkillsManager.getSkill(skillUrn);
+
+        UIImage image = new UIImage(Assets.getTextureRegion("EventualSkills:EventualSkills#skill").get());
+        UILabel label = new UILabel(String.valueOf(skillDescriptionComponent.rank));
+        OverlapLayout layout = new OverlapLayout();
+        layout.addWidget(image);
+        layout.addWidget(label);
+        layout.setTooltip(skillDescriptionComponent.name);
+        layout.setTooltipDelay(0);
+
+        return layout;
+    }
+}
