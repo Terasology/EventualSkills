47ee15ac08f9a065f7eb437c73a3c904e7b787d7
Nail Khanipov <foxflameless@gmail.com>
2021-08-25 22:15:54 +0300

    fix(ecs-gestalt): Migrate Components to gestalt's Components. (#15)
    Ref: MovingBlocks/Terasology#4753
    
    Co-authored-by: Michael Pollind <mpollind@gmail.com>
    Co-authored-by: Tobias Nett <skaldarnar@googlemail.com>

--- src/main/java/org/terasology/eventualSkills/components/EntityEventualSkillsComponent.java
+++ src/main/java/org/terasology/eventualSkills/components/EntityEventualSkillsComponent.java
@@ -1,27 +1,14 @@
-/*
- * Copyright 2015 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2021 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 package org.terasology.eventualSkills.components;
 
 import com.google.common.collect.Maps;
-import org.terasology.engine.entitySystem.Component;
 import org.terasology.engine.network.Replicate;
+import org.terasology.gestalt.entitysystem.component.Component;
 
 import java.util.Map;
 
-public class EntityEventualSkillsComponent implements Component {
+public class EntityEventualSkillsComponent implements Component<EntityEventualSkillsComponent> {
     @Replicate
     public String currentSkillInTraining;
     @Replicate
@@ -38,4 +25,16 @@ public class EntityEventualSkillsComponent implements Component {
     // a map of the skill and how many skill points have already been acquired.  These must always be lowercased, otherwise chaos.
     @Replicate
     public Map<String, Integer> partiallyLearnedSkills = Maps.newHashMap();
+
+    @Override
+    public void copyFrom(EntityEventualSkillsComponent other) {
+        this.currentSkillInTraining = other.currentSkillInTraining;
+        this.currentSkillLevelInTraining = other.currentSkillLevelInTraining;
+        this.currentSkillRankInTraining = other.currentSkillRankInTraining;
+        this.currentTrainingTargetSkillPoints = other.currentTrainingTargetSkillPoints;
+        this.currentTrainingCurrentSkillPoints = other.currentTrainingCurrentSkillPoints;
+        this.trainingLastTimeComputedSkillPoints = other.trainingLastTimeComputedSkillPoints;
+        this.partiallyLearnedSkills.clear();
+        this.partiallyLearnedSkills.putAll(other.partiallyLearnedSkills);
+    }
 }
